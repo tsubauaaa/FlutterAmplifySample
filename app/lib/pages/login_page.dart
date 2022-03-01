dc3f27e1af489ae5c23103b6505947f77c283310
tsubauaaa <tsubasa1173@gmail.com>
2022-03-01 10:17:14 +0900

    text editing controllerをprovider化した

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -1,44 +1,40 @@
 import 'package:app/providers/auth_user_provider.dart';
 import 'package:app/providers/auth_repository_provider.dart';
+import 'package:app/providers/login_text_controller_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends ConsumerStatefulWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends ConsumerState<LoginPage> {
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: SafeArea(
         minimum: const EdgeInsets.symmetric(horizontal: 40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _loginForm(),
+            _loginForm(ref),
           ],
         ),
       ),
     );
   }
 
-  Widget _loginForm() {
+  Widget _loginForm(ref) {
+        final usernameController = ref.watch(usernameControllerProvider);
+    final passwordController = ref.watch(passwordControllerProvider);
+
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
       TextField(
-        controller: _usernameController,
+        controller: usernameController,
         decoration: const InputDecoration(
             labelText: 'Username', icon: Icon(Icons.mail)),
         keyboardType: TextInputType.visiblePassword,
       ),
       TextField(
-        controller: _passwordController,
+        controller: passwordController,
         decoration: const InputDecoration(
             labelText: 'Password', icon: Icon(Icons.lock_open)),
         obscureText: true,
@@ -50,15 +46,15 @@ class _LoginPageState extends ConsumerState<LoginPage> {
           style: TextStyle(color: Colors.black45),
         ),
         onPressed: () {
-          _login();
+          _login(ref, usernameController, passwordController);
         },
       ),
     ]);
   }
 
-  Future<void> _login() async {
-    final username = _usernameController.text.trim();
-    final password = _passwordController.text.trim();
+  Future<void> _login(ref, usernameController, passwordController) async {
+    final username = usernameController.text.trim();
+    final password = passwordController.text.trim();
     final authRepoProvider = ref.watch(authRepositoryProvider);
     await authRepoProvider.signIn(username, password);
     ref.refresh(authUserProvider);
